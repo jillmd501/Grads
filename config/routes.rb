a8d7d410302a87c5d70340afc77a0c502c39e92f
Jill Donohue <jillmd501@gmail.com>
2016-05-02 16:29:31 -0600

    Add styling to main page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
+  get  '/about', to: 'welcome#about'
 end
