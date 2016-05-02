94015db0c1d9b184649b271494aaf25e0fbb0938
Jill Donohue <jillmd501@gmail.com>
2016-05-02 15:10:29 -0600

    Added slider to main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root to: "welcome#index"
+
   resources :users, only: [:new, :create, :show]
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+
 end
