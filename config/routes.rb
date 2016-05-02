38cf7f3eec08978d64ce28e6bf76796d9ae2fc01
Jill Donohue <jillmd501@gmail.com>
2016-05-02 17:14:00 -0600

    Change theme color to orange

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
 
-  resources :users, only: [:new, :create, :show]
-
+  resources :users
+  
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
