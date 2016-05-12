3b084f12b34bbf04685b13c012656db7937eb5ad
Jill Donohue <jillmd501@gmail.com>
2016-05-12 15:19:45 -0600

    fix user authentication crete method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,13 @@ class UsersController < ApplicationController
   end
 
   def create
-  @user = User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to @user
     else
-      #implement later
+      flash.now[:error] = "Invalid Login"
+      redirect_to new_user_path
     end
   end
 
